b35b5613659997091d79282899a5a98e21793689
Adam Carleton <acarleton15@gmail.com>
2026-04-03 20:35:37 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import path from 'path';
+import menuData from './data/menu.json' with { type: 'json' };
+
+const app = express();
+const PORT = 3000;
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(import.meta.dirname, 'views'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+})
+
+app.get('/menu', (req, res) => {
+    res.render('menu.ejs', { menu: menuData });
+})
+
+app.get('menu/:menuItem', (req, res) => {
+    const item = req.params.menuItem;
+    res.render(`menu/${item}`)
+})
+
+app.get('/random', (req, res) => {
+    
+    const menuLength = Object.keys(menuData).length;
+    const randCategory = Math.floor(Math.random() * menuLength);
+    const category = Object.values(menuData);
+    const randItems = [...category[randCategory]];
+    res.render('random.ejs', { menuLength: menuLength, randCategory: randCategory, 
+                                category: category, randItems: randItems });
+})
+
+app.listen(PORT, (req, res) => {
+    console.log(`LISTENING ON PORT ${PORT}`);
+})
